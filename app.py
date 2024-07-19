3c8e98711d7849b6d56e5a2f800bd3772b9c27a8
Valente <evmendezs@gmail.com>
2024-07-19 13:32:07 +0200

    Bug fixes

--- app.py
+++ app.py
@@ -246,7 +246,7 @@ def calculate_total_population(data, continent, country, start_year, end_year, i
     return total_population
 
 # Streamlit app
-st.title('Migratory Movements Analysis Netherlands')
+st.title('Migratory Movements Analysis NL')
 st.markdown('*Note: Figures exclude Migration background = Dutch background*') 
 
 # Data type selection
